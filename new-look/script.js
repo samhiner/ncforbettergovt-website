fd92ed2b50c5e472a04717a90c6e2360b476f215
JacksonMeadeUNC <meade24j@email.unc.edu>
2022-02-07 13:32:26 -0500

    added new look

---
+++ new-look/script.js
@@ -0,0 +1,23 @@
+document.documentElement.setAttribute('data-theme', (window.localStorage.getItem('data-theme')) ?? "light");
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    window.localStorage.setItem('data-theme', (document.documentElement.getAttribute('data-theme')));
+}
+
+$(document).ready(function() {
+    $(".theme-toggler").click(function() {
+        (document.documentElement.getAttribute('data-theme') == 'dark') ?
+        document.documentElement.setAttribute('data-theme', 'light') :
+        document.documentElement.setAttribute('data-theme', 'dark');
+
+        window.localStorage.setItem('data-theme', (document.documentElement.getAttribute('data-theme')));
+    });
+});
+
+$(document).ready(function(){
+    $(".hamburger").click(function(){
+      $(this).toggleClass("is-active");
+      $('.fullscreen-nav').toggleClass("is-active");
+    });
+  });
