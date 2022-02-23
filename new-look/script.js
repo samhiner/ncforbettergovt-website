145d456cf82be51ad50b36a668318c231a2d5345
JacksonMeadeUNC <meade24j@email.unc.edu>
2022-02-23 15:20:42 -0500

    Push new look

--- new-look/script.js
+++ new-look/script.js
@@ -1,10 +1,5 @@
 document.documentElement.setAttribute('data-theme', (window.localStorage.getItem('data-theme')) ?? "light");
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    window.localStorage.setItem('data-theme', (document.documentElement.getAttribute('data-theme')));
-}
-
 $(document).ready(function() {
     $(".theme-toggler").click(function() {
         (document.documentElement.getAttribute('data-theme') == 'dark') ?
